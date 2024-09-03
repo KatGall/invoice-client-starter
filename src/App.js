c0d3eb5465aeb35db12f85b9cd3a6d93e4f896f7
KatGall <166738965+KatGall@users.noreply.github.com>
2024-09-03 19:26:24 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import PersonIndex from "./persons/PersonIndex";
+import PersonDetail from "./persons/PersonDetail";
+import PersonForm from "./persons/PersonForm";
+
+export function App() {
+  return (
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/persons"} className="nav-link">
+                Osoby
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route index element={<Navigate to={"/persons"} />} />
+          <Route path="/persons">
+            <Route index element={<PersonIndex />} />
+            <Route path="show/:id" element={<PersonDetail />} />
+            <Route path="create" element={<PersonForm />} />
+            <Route path="edit/:id" element={<PersonForm />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
